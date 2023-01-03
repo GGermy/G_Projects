2e3e5ccfe259dc5ef3a40f7a9d51986214453061
GGermy <112440396+GGermy@users.noreply.github.com>
2023-01-03 09:44:56 -0800

    totally know how to count

--- ForgotCalculator.java
+++ ForgotCalculator.java
@@ -6,7 +6,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
